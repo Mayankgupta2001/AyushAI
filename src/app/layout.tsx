fcf721fb69db63ac3b7d2a19d5d903d7c77f6a9f
Mayank Gupta <mg8003987193@gmail.com>
2026-04-06 11:39:52 +0530

    feat: add AdSense publisher ID

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -71,7 +71,7 @@ export default function RootLayout({
       >
         <Script
           async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-XXXXXXXXXXXXXXXX"
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4314810893346292"
           crossOrigin="anonymous"
           strategy="afterInteractive"
         />
