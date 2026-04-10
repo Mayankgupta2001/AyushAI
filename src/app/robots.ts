feeb3010c13c734f46e75e6050d2f377aadb8ef5
Mayank Gupta <mg8003987193@gmail.com>
2026-04-10 12:53:43 +0530

    fix issue in robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://ayushai.vercel.app/sitemap.xml',
+    sitemap: 'https://ayush-ai-t6bo.vercel.app/sitemap.xml',
   };
 }
